2ce54239d51d208273f044e7fe108733128860b2
Ivens magno <ivensmagno@gmail.com>
2025-10-25 17:49:51 -0300

    feat: Adiciona configuração personalizada do OpenAPI/Swagger
    - Cria classe de configuração OpenApiConfig
    - Adiciona informações personalizadas da API

---
+++ springBoot/src/main/java/TaoTaoPerto/springBoot/config/OpenApiConfig.java
@@ -0,0 +1,23 @@
+package TaoTaoPerto.springBoot.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.Contact;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("E-commerce TaoTaoPerto Docuemntação")
+                        .description("Documentação de APIs para o e-commerce TaoTaoPerto")
+                        .version("0.0.1")
+                        .contact(new Contact()
+                                .name("Ivens Magno Da Costa Lisboa")
+                                .email("ivensmagno@gmail.com")));
+    }
+}
