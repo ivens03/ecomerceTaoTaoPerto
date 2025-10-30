1e1aff78c0852bbb324a8a1bbbeb434afda9fde9
Ivens Magno <ivensmagno@gmail.com>
2025-10-30 16:18:59 -0300

    doc: atualização da versão no swagger
    - tratamento de erro personalizado

--- springBoot/src/main/java/TaoTaoPerto/springBoot/config/OpenApiConfig.java
+++ springBoot/src/main/java/TaoTaoPerto/springBoot/config/OpenApiConfig.java
@@ -15,7 +15,7 @@ public class OpenApiConfig {
                 .info(new Info()
                         .title("E-commerce TaoTaoPerto Docuemntação")
                         .description("Documentação de APIs para o e-commerce TaoTaoPerto")
-                        .version("0.0.3")
+                        .version("0.0.4")
                         .contact(new Contact()
                                 .name("Ivens Magno Da Costa Lisboa")
                                 .email("ivensmagno@gmail.com")));
