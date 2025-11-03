2f739194f42d05c614371435ab1d7424e7faf68d
Ivens Magno <ivensmagno@gmail.com>
2025-11-03 11:57:50 -0300

    feat: configurações de rotas e criptografia de senha.

---
+++ springBoot/src/main/java/TaoTaoPerto/springBoot/config/security/SecurityConfig.java
@@ -0,0 +1,47 @@
+package TaoTaoPerto.springBoot.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    /**
+     * Bean responsavel por fazer a criptografia usando o BCrypt.
+     */
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    /**
+     * Este é o Bean que configura todas as regras de segurança HTTP.
+     */
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/ususarios/cadastrar").permitAll()
+                        .anyRequest().authenticated()
+                );
+/*                .formLogin(form -> form
+                        .loginPage("/login")
+                        .permitAll()
+                )
+                .logout(logout -> logout
+                        .logoutUrl("/logout")
+                        .logoutSuccessUrl("/login?logout")
+                        .permitAll()
+                );*/
+
+        return http.build();
+    }
+}
