c41f762836d685f39f6b6b215a858c442ad0c3b1
Ivens Magno <ivensmagno@gmail.com>
2025-11-06 11:04:26 -0300

    feat: aprimora a interface de login e registro com melhorias de layout e funcionalidade

--- angular/src/app/login-cliente/login-cliente.ts
+++ angular/src/app/login-cliente/login-cliente.ts
@@ -1,33 +1,28 @@
-// src/app/login/login.component.ts
-
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common'; // IMPORTANTE!
+import { CommonModule } from '@angular/common'; 
 
 @Component({
-  selector: 'app-login', // <--- Essa é a tag HTML do seu componente
-  standalone: true,     // Projetos novos usam 'standalone: true'
-  imports: [CommonModule], // Precisa disso para o [ngClass] e [hidden] funcionarem
+  selector: 'app-login', 
+  standalone: true,    
+  imports: [CommonModule], 
   templateUrl: './login-cliente.html',
   styleUrls: ['./login-cliente.css']
 })
 export class LoginComponent {
 
-  // --- Lógica para alternar Abas ---
-  // Esta variável controla qual aba está ativa.
-  // O HTML lê ela para saber o que mostrar.
   abaAtual: 'login' | 'register' = 'login';
 
-  // --- Lógica para Senha do Login ---
   loginPasswordType: string = 'password';
   loginPasswordIcon: 'open' | 'closed' = 'open';
 
-  // --- Lógica para Senha do Registro ---
   regPasswordType: string = 'password';
   regPasswordIcon: 'open' | 'closed' = 'open';
 
+  regConfirmPasswordType = 'password';
+  regConfirmPasswordIcon = 'closed';
+
   constructor() { }
 
-  // Esta função é chamada pelo (click) no HTML do login
   toggleLoginPassword() {
     if (this.loginPasswordType === 'password') {
       this.loginPasswordType = 'text';
@@ -38,7 +33,6 @@ export class LoginComponent {
     }
   }
 
-  // Esta função é chamada pelo (click) no HTML do registro
   toggleRegPassword() {
     if (this.regPasswordType === 'password') {
       this.regPasswordType = 'text';
@@ -49,17 +43,18 @@ export class LoginComponent {
     }
   }
 
-  // Esta função impede o form de recarregar a página
   onFormSubmit(event: Event) {
-    event.preventDefault(); // Impede o recarregamento
+    event.preventDefault(); 
     
     if (this.abaAtual === 'login') {
       console.log('Formulário de Login enviado!');
-      // Aqui você colocaria a lógica de login real
     } else {
       console.log('Formulário de Registro enviado!');
-      // Aqui você colocaria a lógica de registro real
     }
   }
 
+    toggleRegConfirmPassword(): void {
+    this.regConfirmPasswordType = this.regConfirmPasswordType === 'password' ? 'text' : 'password';
+    this.regConfirmPasswordIcon = this.regConfirmPasswordIcon === 'open' ? 'closed' : 'open';
+  }
 }
