20475fb754057cbbee9bd241c77b6e77a991a4e5
Ivens Magno <ivensmagno@gmail.com>
2025-11-06 16:40:30 -0300

    feat - "Metados de busca melhores"
    - Implementa dois novos metados de busca sendo um para buscar por id e um para todos.

--- springBoot/src/main/java/TaoTaoPerto/springBoot/produtos/repository/CategoriaRepository.java
+++ springBoot/src/main/java/TaoTaoPerto/springBoot/produtos/repository/CategoriaRepository.java
@@ -4,6 +4,11 @@ import TaoTaoPerto.springBoot.produtos.model.CategoriaModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface CategoriaRepository extends JpaRepository<CategoriaModel, Long> {
+    Optional<CategoriaModel> findByIdAndAtivoTrue(Long id);
+    List<CategoriaModel> findAllByAtivoTrue();
 }
